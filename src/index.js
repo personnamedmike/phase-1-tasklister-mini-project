a5fb72e46e9cd2ebc1a62c8d601189d4daacda7b
Mike Starr <imhs@protonmail.com>
2022-01-17 13:45:07 -0800

    Basic tasks completed for todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // extract inputted value
+    const task = e.target["new-task-description"].value;
+
+    // append x button as childElement to ul
+    const button = document.createElement("button");
+    button.textContent = "x";
+
+    const li = document.createElement("li");
+    li.textContent = `${task} `;
+    li.appendChild(button);
+
+    button.addEventListener("click", removeTask);
+    const ul = document.querySelector("ul");
+    ul.append(li);
+
+    form.reset();
+  });
 });
+
+function removeTask() {
+  this.parentNode.remove();
+}
